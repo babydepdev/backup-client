ba35c21f56595af7fdd62a357abe470bd0adfebd
babydev <babydev.weangkom@gmail.com>
2025-03-27 22:56:22 +0700

    Update Form.jsx

--- src/Form.jsx
+++ src/Form.jsx
@@ -4,12 +4,11 @@ import axios from "axios";
 const Form = () => {
   const [payload, setPayload] = useState({
     path: "",
-    moveTo: "",
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await axios.post("http://localhost:3000/backup", payload);
+    const res = await axios.post("http://localhost:8000/backup", payload);
     console.log(res?.data);
   };
 
@@ -20,11 +19,6 @@ const Form = () => {
           type="text"
           placeholder="Enter your Path"
           onChange={(e) => setPayload({ ...payload, path: e.target.value })}
-        />
-        <input
-          type="text"
-          placeholder="Enter your Move To"
-          onChange={(e) => setPayload({ ...payload, moveTo: e.target.value })}
         />
         <button type="submit">Submit</button>
       </form>
